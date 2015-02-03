ff0f524885eabd92ad4a06ae6493d6a0d5fdf0d9
jeparson <jerod.parsons@nist.gov>
2015-02-03 14:35:18 -0800

    Adding count tables.

--- 140318-SEQClmEst4dist.R
+++ 140318-SEQClmEst4dist.R
@@ -1,15 +1,16 @@
-#More "generalized" code to solve for unknown mixture proportions. 
-#first  Get the mrna fraction
-#currently works for both SEQC and BLM data!!! and arbitrary numbers of mixes (given a little massaging to get the input format right)
+#More "generalized" code to solve for unknown mixture proportions.
+#currently works for both SEQC and BLM data, with arbitrary numbers of mixes
+
+
 GeneralLMest<-function(infile,spikeID="ERCC-",components=c("bep","lep","mep"),mixes=c("a1"),spikemassfraction=c(0.003,0.003,0.003)){
-##formatting of infile:
+###IMPORTANT###formatting of infile:
   # Rownames:  gene_ids, with a phrase ("spikeID") that identifies Spike-In controls
   # Columns: 1 column per component, 1 column per mix
   #must input list of components and list of mixes: For example:  the BLM mix contains components= c("bep","lep","mep") mixes=c("a1","a2")
   #spikemassfraction is the mass proportion you targeted with your spike-in controls
   #input should already be averaged across replicates
 
-mfrac<-calcmrnafracgeneral(infile[,components],spikeID,spikemassfraction)  
+mfrac<-calcmrnafracgeneral(infile[,components],spikeID,spikemassfraction)
 #Mfrac goes into calcmrnafracgeneral, which calculates the mRNA fraction of each sample in the table using equation 3 - here we send it only the "components" of the mixture.
 if(length(mfrac)!=length(components)){stop("ERROR:  Number of components != Number of calculated mRNA fractions!")}
 {mixval<-NULL;rdf<-NULL}#set up dummy variables
@@ -24,17 +25,14 @@ for(I in 2:length(components)){
 mixval<-coefficients(lm(data=infile, as.formula(paste(mixtext,"~",modeltext))))/sum(coefficients(lm(data=infile,as.formula(paste(mixtext,"~",modeltext)))))
 #Mixval for a mixture sample in "mixes" is here determined to be the coefficients of a linear model fit to the Identity "I" of the mRNA fraction "mfrac" value of a component in "components"
 #times the mixture expression of that component, plus the same value for all other components.  This is equation 2.  There is no intercept fit (+0) for this model.
-
 rdf<-rbind(rdf,mixval)
-
-
  }
 rownames(rdf)<-mixes
 colnames(rdf)<-components
 return(rdf)
 }
 }
-#calculates the mRNA fraction of a given count table and returns the normalized component values.  
+#calculates the mRNA fraction of a given count table and returns the normalized component values.
 #thisfileneedstobeaddedtothegitrepo##
 calcmrnafracgeneral<-function(dat,spikeID,spikemassfraction=0.05){
   #1) Identify which counts are Spike-In and which are not
